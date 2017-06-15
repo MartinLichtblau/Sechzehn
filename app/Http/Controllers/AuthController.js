b6fa054c7651f470b4f93d605accd101c0272fbc
Tim Walter <tim@twwd.de>
2017-06-15 10:48:07 +0200

    Clean up AuthController.login

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -4,15 +4,8 @@ class AuthController {
   * login (request, response) {
     const credentials = request.only('email', 'password')
 
-    console.log(credentials)
-
-    const {
-      email,
-      password
-    } = credentials
-
     try {
-      const token = yield request.auth.attempt(email, password)
+      const token = yield request.auth.attempt(credentials.email, credentials.password)
 
       response.ok({
         token: token
