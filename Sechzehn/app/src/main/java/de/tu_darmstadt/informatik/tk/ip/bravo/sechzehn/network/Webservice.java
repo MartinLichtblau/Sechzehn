e0b20b6785e1cebac2b37641c0c0bd6503d73b1d
Johannes Semsch <jojo.semsch@gmail.com>
2017-06-21 17:24:09 +0200

    add retrofit & gson to gradle start working on api connection

---
+++ Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/network/Webservice.java
@@ -0,0 +1,13 @@
+package de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.network;
+
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * Created by JohannesTP on 21.06.2017.
+ */
+
+//public interface Webservice {
+//    @POST("/users")
+//    Call<UserToken> createUser(@Body User user);
+//}
