344495daf792af93cab5056cce121557668d86aa
Alexander Geiß <alexander@geiss1.de>
2017-06-21 23:06:16 +0200

    Downgrade GooglePlay Services Add API data classes

--- Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/network/User.java
+++ Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/network/User.java
@@ -1,9 +1,37 @@
 package de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.network;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by JohannesTP on 21.06.2017.
  */
 
-//public class User {
+public class User {
+    public int id;
 
-//}
+    public String email;
+
+    public String username;
+
+    public String password;
+
+    @SerializedName("password_confirmation")
+    public String passwordConfirmation;
+
+    @SerializedName("real_name")
+    public String realName;
+
+    public String city;
+
+    @SerializedName("profile_picture")
+    public String profilePicture;
+
+    public double lat;
+
+    public double lng;
+
+    public LatLng getLatLng() {
+        return new LatLng(lat, lng);
+    }
+}
--- Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/network/Webservice.java
+++ Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/network/Webservice.java
@@ -1,5 +1,6 @@
 package de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.network;
 
+import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
@@ -7,7 +8,8 @@ import retrofit2.http.POST;
  * Created by JohannesTP on 21.06.2017.
  */
 
-//public interface Webservice {
-//    @POST("/users")
-//    Call<UserToken> createUser(@Body User user);
-//}
+public interface Webservice {
+    @POST("/users")
+    Call<UserToken> createUser(@Body User user);
+
+}
