c0daf96ba728a4f86bbf12d35f48d27d2a6e944d
Martin Lichtblau <martin.lichtblau@gmail.com>
2017-07-06 07:41:55 +0200

    Does not crash anymore and I do not know why

--- Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/fragments/UserProfileFragment.java
+++ Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/fragments/UserProfileFragment.java
@@ -10,17 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
-
 import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.R;
 import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.data.User;
 import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.databinding.FragmentProfileUserBinding;
@@ -33,8 +29,9 @@ public class UserProfileFragment extends BaseFragment implements OnMapReadyCallb
     private ImageView optionsButton;
     private static final String USERNAME = "username";
     private UserProfileViewModel viewModel;
-
+    private FragmentProfileUserBinding binding;
     SupportMapFragment mapFragment;
+    GoogleMap mMap;
 
     public static UserProfileFragment newInstance(String username) {
         Bundle bundle = new Bundle();
@@ -47,22 +44,55 @@ public class UserProfileFragment extends BaseFragment implements OnMapReadyCallb
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //View view = inflater.inflate(R.layout.fragment_profile_user, container, false);
-        final FragmentProfileUserBinding binding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_profile_user, container, false);
-        binding.setUser(new User());
-
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_user, container, false);
         addFriendButton = binding.userprofileAddfriend;
         optionsButton = binding.userprofileOptions;
 
         viewModel = ViewModelProviders.of(this).get(UserProfileViewModel.class);
         if (viewModel.getUser().getValue() == null)
             viewModel.initUser(getArguments().getString("username"));
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getChildFragmentManager().beginTransaction()
+                .remove(mapFragment)
+                .commit();
+        mMap = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setup();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        addFriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    private void setup(){
         viewModel.getUser().observe(this, new Observer<User>() {
             @Override
             public void onChanged(User user) {
@@ -71,56 +101,12 @@ public class UserProfileFragment extends BaseFragment implements OnMapReadyCallb
                     //Picasso.with(getActivity()).setLoggingEnabled(true);
                     Picasso.with(getActivity()).load("http://"+user.getProfilePicture()).transform(new RoundedCornersTransformation(10,10)).into(binding.userprofilePicture); //Picasso needs "http://"
                 }
-            }
-        });
-
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
-        //
-
-        mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-      return binding.getRoot();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        getChildFragmentManager().beginTransaction()
-                .remove(mapFragment)
-                .commit();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        addFriendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModel.initUser("parru");
-            }
-        });
-
-        optionsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                LatLng pos = new LatLng(viewModel.getUser().getValue().getLat(),viewModel.getUser().getValue().getLng());
+                mMap.addMarker(new MarkerOptions().position(pos)
+                        .title(getArguments().getString("username")));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 10));
             }
         });
     }
-
 }
 
