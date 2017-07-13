593def9cfd4a73ed3222661191fa6cfb4995c5d1
Tim Walter <tim@twwd.de>
2017-07-13 13:31:54 +0200

    Add Controller stubs

---
+++ app/Http/Controllers/VenueCategoryController.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class VenueCategoryController {
+  * index (request, response) {
+    //
+  }
+
+  * show (request, response) {
+    //
+  }
+
+  * store (request, response) {
+    //
+  }
+
+  * update (request, response) {
+    //
+  }
+
+  * destroy (request, response) {
+    //
+  }
+}
+
+module.exports = VenueCategoryController
---
+++ app/Http/Controllers/VenueController.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class VenueController {
+  * index (request, response) {
+    //
+  }
+
+  * show (request, response) {
+    //
+  }
+
+  * store (request, response) {
+    //
+  }
+
+  * update (request, response) {
+    //
+  }
+
+  * destroy (request, response) {
+    //
+  }
+}
+
+module.exports = VenueController
