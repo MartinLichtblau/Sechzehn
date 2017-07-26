8903d347a1a759aac427758533e521fc65215c42
Martin Lichtblau <martin.lichtblau@gmail.com>
2017-07-26 14:45:16 +0200

    Fix Bug: FragNavController is null in first fragment. Comment: Still null right after start.

--- Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/fragments/BaseFragment.java
+++ Sechzehn/app/src/main/java/de/tu_darmstadt/informatik/tk/ip/bravo/sechzehn/fragments/BaseFragment.java
@@ -3,32 +3,17 @@ package de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.fragments;
 import android.arch.lifecycle.LifecycleFragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.google.android.gms.tasks.RuntimeExecutionException;
-import com.ncapdevi.fragnav.FragNavController;
-
 import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.AnimatedFragNavController;
-import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.R;
-import de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.activities.BottomTabsActivity;
 
 /**
  * Created by niccapdevila on 3/26/16.
  */
 public abstract class BaseFragment extends LifecycleFragment {
-
-    public static final String ARGS_INSTANCE = "de.tu_darmstadt.informatik.tk.ip.bravo.sechzehn.argsInstance";
-
-    AnimatedFragNavController mFragmentNavigation;
-    int mInt = 0;
+    NavController mFragmentNavigation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -48,23 +33,15 @@ public abstract class BaseFragment extends LifecycleFragment {
         if(mFragmentNavigation==null){
             throw new NullPointerException("The FragNavController is not available.");
         }
-        return mFragmentNavigation;
+        return mFragmentNavigation.getNavController();
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof NavController) {
-            mFragmentNavigation =((NavController) context).getNavController();
+            mFragmentNavigation =((NavController) context);
         }
-    }
-
-    public String getTextFrom(View v,int id){
-        View v2 = v.findViewById(id);
-        if(!(v.findViewById(id) instanceof TextInputEditText)){
-           throw new RuntimeException("The ID must refer to a TextInputEditText.") ;
-        }
-        return ((TextInputEditText)v.findViewById(id)).getText().toString();
     }
 
     public interface NavController {
