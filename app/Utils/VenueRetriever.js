8e26525c978a7dc06910f5b6c369a4ea5567b2e3
Tim Walter <tim@twwd.de>
2017-08-09 17:19:40 +0200

    WIP Foursquare data retrieval

--- app/Utils/VenueRetriever.js
+++ app/Utils/VenueRetriever.js
@@ -6,6 +6,7 @@ const Venue = use('App/Model/Venue')
 const VenueRetrieval = use('App/Model/VenueRetrieval')
 const Route = use('Route')
 const Config = use('Config')
+const Http = require('http')
 
 class VenueRetriever {
   * retrieve (lat, lng, radius) {
@@ -14,13 +15,47 @@ class VenueRetriever {
 
     const roundedRadius = this.roundRadius(radius)
 
-    console.log(radius)
+    const oldRetrievalCount = yield VenueRetrieval.query().where('lat', roundedLat).where('lng', roundedLng).where('radius', roundedRadius).count()
+
+    /*
+        yield VenueRetrieval.create({
+          lat: roundedLat,
+          lng: roundedLng,
+          radius: roundedRadius
+        })
+    */
+
+    console.log(roundedRadius)
   }
 
+  /**
+   * Round the given radius to the next power of two.
+   * @param radius the radiuss
+   * @returns {number} the power of two
+   */
   roundRadius (radius) {
-    let exp = Math.round(Math.sqrt(radius))
-
+    let exp = Math.ceil(Math.log2(radius))
     return Math.pow(2, exp)
+  }
+
+  * requestFoursquare (lat, lng, radius) {
+    return Http.get({
+      host: 'https://api.foursquare.com',
+      path: '/v2/venues/search'
+    }, function (response) {
+      // Continuously update stream with data
+      let body = ''
+      response.on('data', d => {
+        body += d
+      })
+      response.on('end', () => {
+        this.processFoursquareData(JSON.parse(body))
+      })
+    })
+  }
+
+  * processFoursquareData (data) {
+    console.log(data)
   }
 }
 
